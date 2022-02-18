501b42c7fd22cae5411a7a741d2d6a41d991d594
Federico <codepalace@fastmail.se>
2022-02-18 13:00:17 +0100

    Made yellow caution sign happy

--- SampleCoreData/DataModel/DataController.swift
+++ SampleCoreData/DataModel/DataController.swift
@@ -15,7 +15,7 @@ class DataController: ObservableObject {
     init() {
         container.loadPersistentStores { description, error in
             if let error = error {
-                print("Failed to load data in DataController")
+                print("Failed to load data in DataController \(error.localizedDescription)")
             }
         }
     }
